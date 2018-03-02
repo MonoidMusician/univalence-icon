53d4b4306f6537d5f09543cb17e1db411b684f64
Nicholas Scheel <monoidmusician@gmail.com>
2018-03-01 21:44:14 -0600

    prepare for capture

--- src/Main.purs
+++ src/Main.purs
@@ -3,17 +3,22 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
-import DOM.HTML
+import DOM.HTML (window)
 import DOM.HTML.Types (htmlDocumentToNonElementParentNode)
-import DOM.HTML.Window
-import DOM.Node.NonElementParentNode
+import DOM.HTML.Window (document)
+import DOM.Node.NonElementParentNode (getElementById)
 import DOM.Node.Types (ElementId(..))
 import DOM.Node.Element (setAttribute)
 import Partial.Unsafe (unsafePartial)
-import Data.Maybe
+import Data.Maybe (fromJust)
 import Math ((%))
 import FRP.Behavior (Behavior, animate)
 import FRP.Behavior.Time (seconds)
+import DOM.Event.Types (EventType(..))
+import DOM.Event.EventTarget as EL
+import Control.Monad.Eff.Ref as Ref
+import Unsafe.Coerce (unsafeCoerce)
+import Control.Monad.Eff.Console (logShow)
 
 curve :: State -> String
 curve { v, e, c } =
@@ -29,8 +34,8 @@ curve { v, e, c } =
 
 type State = { v :: Number, c :: Number, h :: Number, e :: Number }
 
-render :: State -> { lower :: String, mid :: String, curved :: String, rot :: String }
-render { v, e, c, h } =
+infos :: State -> { lower :: String, mid :: String, curved :: String, rot :: String }
+infos { v, e, c, h } =
   let across y h = "m 16," <> show y <> " h " <> show h in
   { lower: across (96.0 - 16.0*v) (96.0*h)
   , mid: across (64.0 - 16.0*v) 96.0
@@ -38,8 +43,10 @@ render { v, e, c, h } =
   , rot: "rotate(" <> show (-33.0*e) <> " 16 32)"
   }
 
+rollover = 5.5 :: Number
+
 cyclic :: Behavior Number
-cyclic = seconds <#> \t -> (t*2.5) % 5.5
+cyclic = seconds <#> \t -> (t*2.5) % rollover
 
 pos :: Number -> State
 pos t
@@ -51,16 +58,30 @@ pos t
 
 main :: Eff _ Unit
 main = do
-  d <- window >>= document >>> map htmlDocumentToNonElementParentNode
+  w <- window
+  d <- document w <#> htmlDocumentToNonElementParentNode
   let get i = unsafePartial fromJust <$> getElementById i d
   lower <- get (ElementId "lower")
   mid <- get (ElementId "mid")
   curved <- get (ElementId "curved")
-  let b = cyclic <#> render <<< pos
-  _ <- animate b \scene -> do
-    let set d e = setAttribute "d" d e
-    set scene.lower lower
-    set scene.mid mid
-    set scene.curved curved
-    setAttribute "transform" scene.rot curved
+  let
+    render scene = do
+      let set d e = setAttribute "d" d e
+      set scene.lower lower
+      set scene.mid mid
+      set scene.curved curved
+      setAttribute "transform" scene.rot curved
+    renderAt = pos >>> infos >>> render
+  canceller <- animate cyclic renderAt
+  let astep = 1.0/8.0
+  step <- Ref.newRef (-2.0 * astep)
+  let
+    listener = EL.eventListener \_ -> do
+      s0 <- Ref.readRef step
+      when (s0 < 0.0) canceller
+      Ref.modifyRef step $ (_ + astep) >>> (_ % rollover)
+      s <- Ref.readRef step
+      renderAt s
+      logShow (s / astep)
+  EL.addEventListener (EventType "click") listener false (unsafeCoerce w)
   pure unit
